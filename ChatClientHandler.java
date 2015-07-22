f44638445a77d431e221e8a8a89a73a38227acae
tissya <tissya@asufi.local>
2015-07-22 14:52:59 +0900

    #3　作ったのです
    はたしてこれでできてるでしょうか？

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -15,3 +15,61 @@ public class ChatClientHandler extends Thread{
 		this.name = ("undefined" + id);
 		
 	}
+	
+	public void run(){
+		try{
+			boolean lp = true;
+			open();
+			while(lp){
+				String[] command = receive();
+				System.out.printnln(name + " : " + command[0]);
+				
+				if(command != null){
+					switch(command[0]){
+						case "help":
+							help();
+							break;
+							
+						case "whoami":
+							whoAmI();
+							break;
+						
+						case "name":
+							if(command.length != 2){
+								send("command format error : name <NewName>");
+								break;
+							}
+							name(command[1]);
+							break;
+							
+						case "post":
+							if(command.length != 2){
+								send("command format error : name <message>");
+								break;
+							}
+							post(command[1]);
+							break;
+							
+						case "help":
+							help();
+							break;
+						
+						case "bye":
+							lp = false;
+							bye();
+							break;
+						
+						default:
+							send(command[0] + " : unknown command");
+							break;
+					}
+				}
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally{
+			handler.remove(this);
+			close();
+		}
+	}
+}
