1d1ed5a5e0f985f3aaaa6660fcaca37741c30d28
tekitoni <MyHome@MacBookAir-ver2012.local>
2015-07-22 14:54:43 +0900

    open、closeメソッドのみ
    作成のみしたんで、関数の呼び出しなどは任せます。

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -15,3 +15,22 @@ public class ChatClientHandler extends Thread{
 		this.name = ("undefined" + id);
 		
 	}
+	
+	private void open() throws IOException {  //クライアントとのデータのやり取りをするストリームを開くメソッド
+		//ストリームを開く
+		InputStream socketIn = socket.getInputStream();
+		OutputStream socketOut = socket.getOutputStream();
+		this.in = new BufferedReader(new InputStreamReader(socketIn));      
+		this.out = new BufferedWriter(new OutputStreamWriter(socketOut));
+	}
+
+
+	 */
+	private void close() {
+		if (in != null) {try { in.close(); } catch (IOException e) { } }
+		if (out != null) {try { out.close(); } catch (IOException e) { } }
+		if (socket != null) {try { socket.close(); } catch (IOException e) { } }
+	}
+
+
+}
